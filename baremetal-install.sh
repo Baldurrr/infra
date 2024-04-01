0e944bb3c0ccbc2f3630ecefe00b9740c4d96498
Baldurrr <77190420+Baldurrr@users.noreply.github.com>
2024-04-01 17:17:50 +0200

    Update baremetal-install.sh

--- baremetal-install.sh
+++ baremetal-install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-BLUE=`tput setaf 4`
-YELLOW=`tput setaf 3`
-WHITE=`tput setaf 7`
-RESET=`tput sgr0`
-
 # Upgrade Docker-compose
-echo "${YELLOW}# Updating repo list #${RESET}"
+echo "# Updating repo list #"
 apt update && apt upgrade -y
 apt install apt-transport-https software-properties-common  ca-certificates gnupg lsb-release -y
 
@@ -16,15 +9,16 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 
-echo "${GREEN}OK!${RESET}"
+echo "OK!"
 echo -e "\n"
 
-echo "${YELLOW}# Installing docker #${RESET}"
+echo "# Installing docker #"
 apt-get update -y
 apt-get install docker-ce docker-ce-cli -y
+echo "OK!"
+echo -e "\n"
 
-
-echo "${YELLOW}# Installing docker-compose #${RESET}"
+echo "# Installing docker-compose #"
 wget https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64
 
 chmod +x docker-compose-linux-x86_64
@@ -32,9 +26,9 @@ mv docker-compose-linux-x86_64 /usr/bin/docker-compose
 
 docker-compose version
 
-echo "${GREEN}OK!${RESET}"
+echo "OK!"
 echo -e "\n"
 
-echo "${YELLOW}# Rebooting in 10s #${RESET}"
+echo "# Rebooting in 10s #"
 sleep 10
 reboot now
